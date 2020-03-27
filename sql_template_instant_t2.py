f7c919e0a36720d3b36118aa057b856736386a28
Jean-Christophe Cazes <jean-christophe.cazes@groupeseloger.com>
2020-03-27 18:04:14 +0100

    begin rework on between

--- sql_template_instant_t2.py
+++ sql_template_instant_t2.py
@@ -3,15 +3,26 @@ SELECT {full_cols} FROM
 
 
       (
-           SELECT {dim_annonce_cols}
-                  from dwhdweb.dimannoncepublicationhisto
-                  WHERE (datedebut >=   TIMESTAMP '{date_t}' AND
-                        (iddwh_typepublication = {typepublication} AND iddwh_typepublicationcouplage = {sourcecouplage}))
+            SELECT DISTINCT(table_max."iddwh_annonce") AS iddwh_annonce, DATE("datedebut") AS datedebut_ref
+
+                    FROM "dwhdweb"."dimannoncepublicationhisto"
+
+            INNER JOIN
+
+            (
+            SELECT "iddwh_annonce", MAX(datedebut) datemaj
+
+                   FROM "dwhdweb"."dimannoncepublicationhisto" WHERE datedebut > timestamp '2020-03-01'
+
+            GROUP BY "iddwh_annonce"
+            )
+            AS table_max
+
+            ON table_max."iddwh_annonce" = "dwhdweb"."dimannoncepublicationhisto"."iddwh_annonce" AND "dwhdweb"."dimannoncepublicationhisto".datedebut = table_max.datemaj
       )
       AS t1
 
-
-LEFT JOIN
+INNER JOIN
 
 
      (
@@ -19,17 +30,37 @@ LEFT JOIN
                       FROM
                          (
                           SELECT
-                               {annonce_cols},
+                               {dim_annonce_cols} ,
+                               DATE("datedebut") AS datedebut_ref
+
+                               FROM "dwhdweb"."dimannoncepublicationhisto"
+                         )
+                      WHERE datedebut_ref >= TIMESTAMP '2020-03-01' AND (iddwh_typepublication = {} AND iddwh_typepublicationcouplage = {})
+
+    )
+    AS full_dwh
+
+ON t1.iddwh_annonce = full_dwh.iddwh_annonce AND full_dwh."datedebut_ref" = t1.datedebut_ref
+
+INNER JOIN
+
+
+     (
+           SELECT *
+                      FROM
+                         (
+                          SELECT
+                               {annonce_cols} ,
                                CAST(from_iso8601_timestamp(CONCAT(CAST(year AS VARCHAR),'-',LPAD(CAST(month AS VARCHAR),2,'0'),'-',LPAD(CAST(day AS VARCHAR),2,'0'))) AS TIMESTAMP) AS datemaj
 
                                FROM "immobc"."annonce_history"
                          )
+                      WHERE datemaj >= TIMESTAMP '{date_t}'
 
-                      WHERE datemaj = TIMESTAMP '{date_t}'
     )
     AS t2
 
-ON t1.iddwh_annonce = t2.{idannonce_join_for_annonce}
+ON t1.iddwh_annonce = t2.{idannonce_join_for_annonce} AND t2."datemaj" = t1.datedebut_ref
 
 INNER JOIN
 
@@ -38,16 +69,16 @@ INNER JOIN
            SELECT *
                       FROM
                           (
-                          SELECT  {bien_cols},
+                          SELECT  idannonce, si_meuble,
                                   CAST(from_iso8601_timestamp(CONCAT(CAST(year AS VARCHAR),'-',LPAD(CAST(month AS VARCHAR),2,'0'),'-',LPAD(CAST(day AS VARCHAR),2,'0'))) AS TIMESTAMP) datemaj
 
-                                  FROM {typebien_table}
+                                  FROM immobc.abmaison_history
                           )
-                      WHERE datemaj = TIMESTAMP '{date_t}'
+                      WHERE datemaj >= TIMESTAMP '2020-03-01'
     )
      AS t3
 
-ON t1.iddwh_annonce = t3.{idannonce_join_for_typebien} AND t3.datemaj = t2.datemaj
+ON t1.iddwh_annonce = t3.idannonce AND t3.datemaj = t2.datemaj
 
 INNER JOIN
 
@@ -55,15 +86,15 @@ INNER JOIN
            SELECT *
                       FROM
                           (
-                          SELECT {transaction_cols},
+                          SELECT idannonce, px, prixmaxi,
                                   CAST(from_iso8601_timestamp(CONCAT(CAST(year AS VARCHAR),'-',LPAD(CAST(month AS VARCHAR),2,'0'),'-',LPAD(CAST(day AS VARCHAR),2,'0'))) AS TIMESTAMP) datemaj
 
-                                  FROM {typetransaction_table}
+                                  FROM immobc.atvente_history
                           )
-                      WHERE datemaj = TIMESTAMP '{date_t}'
+                      WHERE datemaj >= TIMESTAMP '2020-03-01'
     )
 
 AS t4
 
-ON t1.iddwh_annonce = t4.{idannonce_join_for_typetransaction} AND t4.datemaj = t2.datemaj
+ON t1.iddwh_annonce = t4.idannonce AND t4.datemaj = t2.datemaj
 """
